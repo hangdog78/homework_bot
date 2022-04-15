5cd2a93ff4e558ccc9e98599e161ba0eab09246a
hangdog <hangdog@mail.ru>
2022-04-15 13:30:52 +0400

    final

--- homework.py
+++ homework.py
@@ -4,11 +4,10 @@ import requests
 import sys
 import time
 
-from exceptions import (
-    Non200ResponseException,
-)
-from telegram import Bot
+from exceptions import Non200ResponseException
 from logging import StreamHandler
+from requests import RequestException
+from telegram import Bot, TelegramError
 from telegram.ext import CommandHandler, Updater
 
 from dotenv import load_dotenv
@@ -18,9 +17,8 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv('YANDEX_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
-
-RETRY_TIME = 600
-DAY_IN_SEC = 86400
+TELEGRAM_RETRY_TIME = 600
+TELEGRAM_DAY_IN_SEC = 86400
 
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
@@ -50,32 +48,35 @@ def send_message(bot, message):
             text=message
         )
         logger.info(f'Отправлено сообщение: {message}')
-    except Exception as error:
-        logger.error(f'Ошибка отправки сообщения: {error}')
+    except TelegramError as error:
+        logger.exception(f'Ошибка отправки сообщения: {error}')
 
 
-def get_api_answer(current_timestamp):
+def get_api_answer(timestamp):
     """Получение ответа от API Практикум'а."""
-    timestamp = current_timestamp
-    params = {'from_date': timestamp}
-    api_resp = requests.get(ENDPOINT,
-                            headers=HEADERS,
-                            params=params)
+    try:
+        params = {'from_date': timestamp}
+        api_resp = requests.get(ENDPOINT,
+                                headers=HEADERS,
+                                params=params)
 
-    if api_resp.status_code != 200:
-        raise Non200ResponseException(api_resp.status_code)
+        if api_resp.status_code != 200:
+            raise Non200ResponseException(api_resp.status_code)
 
-    return api_resp.json()
+        return api_resp.json()
+
+    except RequestException as error:
+        logger.exception(f'API request error: {error}')
 
 
 def check_response(response):
     """Проверка корректности ответа от API и возврат списка работ."""
-    if type(response) is not dict:
+    if not isinstance(response, dict):
         raise TypeError('Ответ API не является словарем')
 
     homeworks = response.get('homeworks')
 
-    if type(homeworks) is not list:
+    if not isinstance(homeworks, list):
         raise TypeError('Список работ не является списком')
 
     if len(homeworks) == 0:
@@ -86,22 +87,20 @@ def check_response(response):
 
 def parse_status(homework):
     """Проверка статуса отправленной работы."""
-    if 'homework_name' not in homework.keys():
+    if 'homework_name' not in homework:
         raise KeyError('Ключ homework_name не найден в ответе')
 
     homework_name = homework.get('homework_name')
 
-    if 'status' not in homework.keys():
+    if 'status' not in homework:
         raise KeyError('Ключ status не найден в ответе')
 
     homework_status = homework.get('status')
-    print(homework_status)
 
-    if homework_status in HOMEWORK_STATUSES.keys():
-        verdict = HOMEWORK_STATUSES[homework_status]
-    else:
+    if homework_status not in HOMEWORK_STATUSES:
         raise KeyError(f'Получен неизвестный статус {homework_status}')
 
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -115,10 +114,10 @@ def check_tokens():
 def check_wrks(update, context):
     """Ручная проверка статуса работ за сутки по команде check."""
     logger.info('/check command recieved.')
-    chat = update.effective_chat
-    context.bot.send_message(chat.id, 'Проверяем новые статусы за сутки...')
     try:
-        response = get_api_answer(int(time.time()) - DAY_IN_SEC)
+        chat = update.effective_chat
+        context.bot.send_message(chat.id, 'Проверяем новые статусы за сутки.')
+        response = get_api_answer(int(time.time()) - TELEGRAM_DAY_IN_SEC)
         home_wrks = check_response(response)
         if len(home_wrks) == 0:
             context.bot.send_message(chat.id, 'Обновленных статусов нет')
@@ -129,16 +128,16 @@ def check_wrks(update, context):
 
     except Exception as error:
         message = f'Service failed: {error}'
-        logger.error(message)
+        logger.exception(message)
 
 
 def main():
     """Основная логика работы бота."""
-    if check_tokens() is False:
+    if not check_tokens():
         logger.critical('Tokens were not found. Service stopped.')
-        return
-    else:
-        logger.info('Tokens are loaded.')
+        sys.exit()
+
+    logger.info('Tokens are loaded.')
 
     try:
         updater = Updater(token=TELEGRAM_TOKEN)
@@ -146,7 +145,7 @@ def main():
         updater.start_polling()
         bot = Bot(token=TELEGRAM_TOKEN)
     except Exception as error:
-        logger.error(f'Network connection failed: {error}')
+        logger.exception(f'Network connection failed: {error}')
 
     else:
         current_timestamp = int(time.time())
@@ -160,15 +159,16 @@ def main():
                     wrk_status = parse_status(wrk)
                     send_message(bot, wrk_status)
                     current_timestamp = response.get('current_date')
-                    time.sleep(RETRY_TIME)
 
             except Exception as error:
                 message = f'Service failed: {error}'
-                logger.error(message)
+                logger.exception(message)
                 if error not in sent_errors:
                     sent_errors.append(error)
                     send_message(bot, message)
-                    time.sleep(RETRY_TIME)
+
+            finally:
+                time.sleep(TELEGRAM_RETRY_TIME)
 
 
 if __name__ == '__main__':
